a7628b4742ae21ac35b7f1b15f08b485ae6c98ea
saqib shah <xaqibshah@gmail.com>
2024-02-10 10:56:41 +0500

    work with elif condition

--- 10_condition.sh
+++ 10_condition.sh
@@ -1,10 +1,34 @@
-#!/bin/bash
+ #!/bin/bash
+
+#for taking user Input
 
 read -p "Enter your Marks: " marks
 
 
-if[[ $marks -gt 50
-	then
-		echo "You are Passed"
-	else
-		echo "You are Fail!!!!"
+#here -gt use for greater then, space and indentend sensitive
+if [[ $marks -gt 50 ]]
+then
+	echo "You are Passed"
+else
+	echo "You are Fail!!!!"
+
+# use fi for ending if condition
+fi
+
+
+
+
+<<comment 
+
+equal 				-eq/==
+greaterthan or equal to 	-ge
+lessthan or equal to 		-le
+notequal			-ne/!=
+greaterthan			-gt
+less than			-lt
+
+comment
+
+
+
+
---
+++ 10a_condition.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+<<comment
+equal 				-eq/==
+Greaterthanorequalto		-ge
+Lessthanorequalto		-le
+NotEqual			-ne/!=
+GreaterThan			-gt
+lessThan			-lt
+
+comment
+
+
+read -p "Enter you marks: " marks
+
+if [ $marks -ge 80 ]
+then
+	echo "First Division"
+elif [ $marks -ge 60 ]
+then
+	echo "Second Division"
+elif [ $marks -ge 40 ]
+then
+	echo "Third Division"
+else
+	echo "Fail"
+fi
